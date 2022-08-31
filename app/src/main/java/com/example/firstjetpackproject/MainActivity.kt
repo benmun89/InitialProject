55581640104771e6c396276aa2d31d9aa7e23d2b
benjamin <ben.mun89@gmail.com>
2022-08-30 20:39:51 -0500

    Change the color of the text to mach the designs

--- app/src/main/java/com/example/firstjetpackproject/MainActivity.kt
+++ app/src/main/java/com/example/firstjetpackproject/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firstjetpackproject.ui.theme.FirstJetpackProjectTheme
 
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", color = Color.Magenta)
 }
 
 @Preview(showBackground = true)
